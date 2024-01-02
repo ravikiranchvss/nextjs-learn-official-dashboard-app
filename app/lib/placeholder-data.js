f425a8053f87ec3593ee11f9d7ea6a23a2350499
Ravi Kiran <rchandra@ftdi.com>
2024-01-02 19:45:44 +0530

    changes till chapter 7

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,12 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a64427',
+    name: 'Rambo',
+    email: 'john@rambo.com',
+    password: 'Rambo404',
+  },
 ];
 
 const customers = [

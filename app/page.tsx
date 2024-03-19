37dfdd1428d8ba144e925434b78db5c670603c38
Ravi Kiran <rchandra@ftdi.com>
2024-03-19 10:49:37 +0530

    log added

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
+  console.info('main page got triggered =================')
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
